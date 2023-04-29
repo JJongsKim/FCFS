3721c21259a3dfa8d8f25288b77fd3b75f1f2b0b
JJongsKim <totnwjd530@gmail.com>
2023-04-29 16:30:59 +0900

    :lipstick: style: 로그인, 회원가입 페이지 UI 스타일링

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,7 +1,32 @@
+import Button from '../../components/common/Button';
+import InputBase from '../../components/common/Input';
+
 import styles from './SignUp.module.scss';
 
 const SignUp = () => {
-  return <h2>회원가입 페이지</h2>;
+  return (
+    <div className={styles.signUpWrap}>
+      <span>
+        <InputBase type="text" label="이름" placeHolder="이름을 입력해주세요!" />
+      </span>
+      <span>
+        <InputBase type="text" label="아이디" placeHolder="아이디를 입력해주세요!" />
+      </span>
+      <span>
+        <InputBase type="password" label="비밀번호" placeHolder="비밀번호를 입력해주세요!" />
+      </span>
+      <span>
+        <InputBase type="password" label="비밀번호 확인" />
+      </span>
+
+      <div className={styles.loginBtnWrap}>
+        <p>이미 가입하셨나요? 로그인하기</p>
+        <Button size="large" color="blue">
+          회원가입
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
